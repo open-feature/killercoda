ef5fe3f619796a5781195c1b6ebd16d25b7077bc
agardnerit <adam@agardner.net>
2022-10-17 11:59:41 +1000

    add tracking
    Signed-off-by: agardnerit <adam@agardner.net>

---
+++ openfeature-demo/assets/scripts/intro_background.sh
@@ -0,0 +1,1 @@
+curl -X POST 'https://95kvmmsko0.execute-api.us-east-1.amazonaws.com/default/openfeature_killercoda_tracker' -H 'Content-Type: text/plain' --data-raw '{ "tutorial": "openfeature","pageName": "intro.md" }'
