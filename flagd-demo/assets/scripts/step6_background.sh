14d20536ce5622414aec28bef4225200d8a56de9
Adam Gardner <adam@agardner.net>
2023-03-22 11:28:14 +1000

    flag tracking background scripts
    Signed-off-by: Adam Gardner <adam@agardner.net>

---
+++ flagd-demo/assets/scripts/step6_background.sh
@@ -0,0 +1,1 @@
+curl -X POST 'https://3aeq2v5gc8.execute-api.us-east-1.amazonaws.com/default/openfeature_flagd_demo_killercoda_tracker' -H 'Content-Type: text/plain' --data-raw '{ "tutorial": "flagd_demo","pageName": "step6.md" }'
