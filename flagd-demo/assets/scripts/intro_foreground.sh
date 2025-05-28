0ddbd6772436face1f4a4adad872494ea5e639e8
bbland1 <104288486+bbland1@users.noreply.github.com>
2025-05-28 16:20:59 -0400

    adding variables for passed info to script
    Signed-off-by: bbland1 <104288486+bbland1@users.noreply.github.com>

--- flagd-demo/assets/scripts/intro_foreground.sh
+++ flagd-demo/assets/scripts/intro_foreground.sh
@@ -4,6 +4,11 @@ DEBUG_VERSION=13
 GITEA_VERSION=1.23.8
 TEA_CLI_VERSION=0.9.2
 FLAGD_VERSION=0.11.5
+USER_NAME="openfeature"
+PASSWORD="openfeature"
+USER_EMAIL=me@faas.com
+TOKEN_NAME="tea_token"
+REPO_NAME="flags"
 
 if [[ -n "${TRAFFIC_HOST1_3000:-}" ]]; then
   BASE_URL="http://${TRAFFIC_HOST1_3000}"
@@ -31,32 +36,32 @@ done
 user_list=$(docker exec -u git gitea gitea admin user list 2>/dev/null)
 
 # Check if openfeature user exists
-if ! echo "$user_list" | grep -qw "openfeature"; then
+if ! echo "$user_list" | grep -qw "$USER_NAME"; then
   # Using the gitea service started with docker
   echo "Creating openfeature admin gitea user..."
   docker exec -u git gitea gitea admin user create \
-    --username=openfeature \
-    --password=openfeature \
-    --email=me@faas.com \
+    --username=$USER_NAME \
+    --password=$PASSWORD \
+    --email=$USER_EMAIL \
     --must-change-password=false
 else
   echo "User already exists. Continuing..."
 fi
 
 echo "Checking for existing token ..."
-user_tokens=$(docker exec gitea curl -s -H "Authorization: Basic $(echo -n "openfeature:openfeature" | base64)" \
-  "$GITEA_URL/api/v1/users/openfeature/tokens")
+user_tokens=$(docker exec gitea curl -s -H "Authorization: Basic $(echo -n "$USER_NAME:$USER_PASSWORD" | base64)" \
+  "$BASE_URL/api/v1/users/$USER_NAME/tokens")
 
 # Output the token check into JSON array & looping to get id of tea_token
-token_id=$(echo "$user_tokens" | jq -r '.[] | select(.name == "tea_token") | .id') > /dev/null
+token_id=$(echo "$user_tokens" | jq -r '.[] | select(.name == $TOKEN_NAME) | .id') > /dev/null
 
 # When the token ID exists delete to regenerate to adhere to gitea usage
 # non-empty && not null
 if [ -n "$token_id" ] && [ "$token_id" != "null" ]; then
   echo "Deleting existing token..."
   docker exec gitea curl -s -X DELETE \
-    "$GITEA_URL/api/v1/users/openfeature/tokens/$token_id" \
-    -H "Authorization: Basic $(echo -n "openfeature:openfeature" | base64)"
+    "$BASE_URL/api/v1/users/$USER_NAME/tokens/$token_id" \
+    -H "Authorization: Basic $(echo -n "$USER_NAME:$USER_PASSWORD" | base64)"
   echo "Re-generating gitea access token for tea CLI..."
 else
   echo "No existing tea_token."
@@ -65,8 +70,8 @@ fi
 
 # Generate access token for tea CLI set up
 docker exec -u git gitea gitea admin user generate-access-token \
-  --username=openfeature \
-  --token-name=tea_token \
+  --username=$USER_NAME \
+  --token-name=$TOKEN_NAME \
   --scopes=all \
   --raw > /tmp/output.log 
 
@@ -89,13 +94,13 @@ tea login add \
 
 # Check if repo 'flags' exists
 echo "Checking if repo 'flags' exists..."
-repo_exists=$(tea repo list --json | jq -e '.[] | select(.name=="flags")' >/dev/null 2>&1 && echo "yes" || echo "no")
+repo_exists=$(tea repo list --json | jq -e '.[] | select(.name==$REPO_NAME)' >/dev/null 2>&1 && echo "yes" || echo "no")
 
 if [[ "$repo_exists" == "yes" ]]; then
   echo "Repo 'flags' already exists. Skipping creation."
 else
   echo "Creating repo 'flags'..."
-  tea repo create --login=local --name=flags --branch=main --init=true >/dev/null
+  tea repo create --login=local --name=$REPO_NAME --branch=main --init=true >/dev/null
 fi
 
 # Add 'git' user
@@ -109,12 +114,12 @@ adduser \
   git
 
 # Configure git for 'ubuntu' and 'git' users
-git config --system user.email "me@faas.com"
-git config --system user.name "OpenFeature"
+git config --system user.email $USER_EMAIL
+git config --system user.name $USER_NAME
 
-git clone http://openfeature:openfeature@${BASE_URL#http://}:3000/openfeature/flags
+git clone http://$USER_NAME:$USER_PASSWORD@${BASE_URL#http://}:3000/$USER_NAME/flags
 
-cd flags
+cd $REPO_NAME
 wget -O example_flags.flagd.json https://raw.githubusercontent.com/open-feature/flagd/main/samples/example_flags.flagd.json
 
 git config credential.helper cache
