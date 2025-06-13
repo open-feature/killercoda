5d71a7ef0be2401537601fb41b3c7bd39f65a246
bbland1 <104288486+bbland1@users.noreply.github.com>
2025-06-13 19:10:14 -0400

    trying to pipe install into next command so install actually works
    Signed-off-by: bbland1 <104288486+bbland1@users.noreply.github.com>

--- five-minutes-to-feature-flags/assets/scripts/intro_foreground.sh
+++ five-minutes-to-feature-flags/assets/scripts/intro_foreground.sh
@@ -23,8 +23,7 @@ source ~/.bashrc
 # -----------------------------------
 # curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 # apt install -y nodejs < /dev/null
-curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
-sudo -E bash nodesource_setup.sh
+curl -fsSL https://deb.nodesource.com/setup_23.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 node -v
 
